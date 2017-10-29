b9cf24ebfab4273b8f81e54fac90278645aeec3d
soni17 <elb004@gmail.com>
2017-10-29 16:03:45 -0400

    wrote method names for all test methods

--- rocket_test.rb
+++ rocket_test.rb
@@ -2,5 +2,38 @@ require "minitest/autorun"
 require_relative "rocket"
 
 class RocketTest < Minitest::Test
-  # Write your tests here!
+
+  def test_name_getter_method
+  end
+
+  def test_name_setter_method
+  end
+
+  def test_colour_getter_method
+  end
+
+  def test_colour_setter_method
+  end
+
+  def test_flying_getter_method
+  end
+
+  def test_lift_off_method_when_rocket_is_flying
+  end
+
+  def test_lift_off_method_when_rocket_is_on_land
+  end
+
+  def test_land_method_when_rocket_is_flying
+  end
+
+  def test_land_method_when_rocket_is_on_land
+  end
+
+  def test_status_method_when_rocket_is_flying
+  end
+
+  def test_status_method_when_rocket_is_on_land
+  end
+
 end
