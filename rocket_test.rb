a555b4ed90e01c87deda7f1879a9f8d8ccae6620
soni17 <elb004@gmail.com>
2017-10-29 21:19:18 -0400

    added comments and organized code

--- rocket_test.rb
+++ rocket_test.rb
@@ -9,8 +9,24 @@ class RocketTest < Minitest::Test
                           flying: true })
   end
 
+  # test for data type assigned to variables
+  def test_colour_setter_method_for_non_strings
+    @rocket.colour = 1245
+    result = @rocket.colour.class
+    expected = String
+    assert_equal(expected, result)
+  end
 
+  def test_name_setter_method_for_non_strings
+    @rocket.name = 1245
+    result = @rocket.name.class
+    expected = String
+    assert_equal(expected, result)
+  end
 
+  
+
+  # test initialize to make sure it assigns the variables if not assigned during instantiation
   def test_initialize_method_assigns_name_when_options_not_provided
     @rocket = Rocket.new()
     result = (@rocket.name != nil)
@@ -32,6 +48,7 @@ class RocketTest < Minitest::Test
     assert_equal(expected, result)
   end
 
+  #test getter and setter methods
   def test_name_getter_method
     result = @rocket.name
     expected = "coolRocket"
@@ -42,13 +59,6 @@ class RocketTest < Minitest::Test
     @rocket.name = "testSetter"
     result = @rocket.name
     expected = "testSetter"
-    assert_equal(expected, result)
-  end
-
-  def test_name_setter_method_for_non_strings
-    @rocket.name = 1245
-    result = @rocket.name.class
-    expected = String
     assert_equal(expected, result)
   end
 
@@ -65,19 +75,13 @@ class RocketTest < Minitest::Test
     assert_equal(expected, result)
   end
 
-  def test_colour_setter_method_for_non_strings
-    @rocket.colour = 1245
-    result = @rocket.colour.class
-    expected = String
-    assert_equal(expected, result)
-  end
-
   def test_flying_getter_method
     result = @rocket.flying?
     expected = true
     assert_equal(expected, result)
   end
 
+  #test lift_off method
   def test_lift_off_method_when_rocket_is_flying
     result = @rocket.lift_off
     expected = false
@@ -91,6 +95,7 @@ class RocketTest < Minitest::Test
     assert_equal(expected, result)
   end
 
+  #test land method
   def test_land_method_when_rocket_is_flying
     result = @rocket.land
     expected = true
@@ -104,6 +109,7 @@ class RocketTest < Minitest::Test
     assert_equal(expected, result)
   end
 
+  #test status method
   def test_status_method_when_rocket_is_flying
     result = @rocket.status
     expected = "Rocket coolRocket is flying through the sky!"
